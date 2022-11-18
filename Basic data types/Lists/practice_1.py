6ec6e06e387d167d73935c5ebcb4b551b10fdd75
Kamran khan <76246927+kamipakistan@users.noreply.github.com>
2022-11-19 00:06:20 +0500

    Update practice_1.py

--- Basic data types/Lists/practice_1.py
+++ Basic data types/Lists/practice_1.py
@@ -46,20 +46,3 @@
 # [1, 5, 9, 10]
 # [9, 5, 1]
 
-
-# Solution
-if __name__ == '__main__':
-    N = int(input())
-    list = []
-    list.insert(0, 5)
-    list.insert(1, 10)
-    list.insert(0, 6)
-    print(list)
-    list.remove(6)
-    list.append(9)
-    list.append(1)
-    list.sort()
-    print(list)
-    list.pop()
-    list.reverse()
-    print(list)
