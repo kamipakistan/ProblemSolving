c0d32aa838a5e32603133499f9e90ca2457c7705
Kamran khan <76246927+kamipakistan@users.noreply.github.com>
2022-12-15 23:52:16 +0500

    Solved

--- solvedProblems/Set Mutations.py
+++ solvedProblems/Set Mutations.py
@@ -1,8 +1,6 @@
 # TASK
 # You are given a set A and N number of other sets. These N number of sets have to perform some specific mutation operations on set A.
-
 # Your task is to execute those operations and print the sum of elements from set A.
-
 # Input Format
 
 # The first line contains the number of elements in set A.
