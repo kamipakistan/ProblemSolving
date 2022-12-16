781be984dcb2ba7bb8df5c45ef4400e00a6fd360
Kamran khan <76246927+kamipakistan@users.noreply.github.com>
2022-12-16 23:41:08 +0500

    Solved
    You are given two sets, A and B.
    Your job is to find whether the set is a subset of set A.
    
    If set A is a subset of set B, print True.
    If set A is not a subset of set B, print False.

--- solvedProblems/Check Subset.py
+++ solvedProblems/Check Subset.py
@@ -1,48 +1,50 @@
-# You are given two sets, A and B.
-# Your job is to find whether set  is a subset of set A.
+"""
+Task
+You are given two sets, A and B.
+Your job is to find whether set  is a subset of set A.
 
-# If set A is subset of set B, print True.
-# If set A is not a subset of set B, print False.
+If set A is subset of set B, print True.
+If set A is not a subset of set B, print False.
 
-# Input Format
+Input Format
 
-# The first line will contain the number of test cases, T.
-# The first line of each test case contains the number of elements in set A.
-# The second line of each test case contains the space separated elements of set A.
-# The third line of each test case contains the number of elements in set B.
-# The fourth line of each test case contains the space separated elements of set B.
+The first line will contain the number of test cases, T.
+The first line of each test case contains the number of elements in set A.
+The second line of each test case contains the space separated elements of set A.
+The third line of each test case contains the number of elements in set B.
+The fourth line of each test case contains the space separated elements of set B.
 
-# Output Format
-# Output True or False for each test case on separate lines.
+Output Format
+Output True or False for each test case on separate lines.
 
-# Sample Input
+Sample Input
 
-# 3
-# 5
-# 1 2 3 5 6
-# 9
-# 9 8 5 6 3 2 1 4 7
-# 1
-# 2
-# 5
-# 3 6 5 4 1
-# 7
-# 1 2 3 5 6 8 9
-# 3
-# 9 8 2
-# Sample Output
+3
+5
+1 2 3 5 6
+9
+9 8 5 6 3 2 1 4 7
+1
+2
+5
+3 6 5 4 1
+7
+1 2 3 5 6 8 9
+3
+9 8 2
+Sample Output
 
-# True 
-# False
-# False
-# Explanation
+True 
+False
+False
+Explanation
 
-# Test Case 01 Explanation
+Test Case 01 Explanation
 
-# Set A = {1 2 3 5 6}
-# Set B = {9 8 5 6 3 2 1 4 7}
-# All the elements of set A are elements of set B.
-# Hence, set A is a subset of set B. 
+Set A = {1 2 3 5 6}
+Set B = {9 8 5 6 3 2 1 4 7}
+All the elements of set A are elements of set B.
+Hence, set A is a subset of set B. """
 
 # ==========================================================================================
 # ====================================== Solution ==========================================
