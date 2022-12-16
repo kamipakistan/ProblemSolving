a751a87f50de89a6b1de50a3e0393f6df5bfc9f8
Kamran khan <76246927+kamipakistan@users.noreply.github.com>
2022-12-16 23:38:16 +0500

    Solved
    You are asked to ensure that the first and last names of people begin with a capital letter in their passports. For example, alison heck should be capitalised correctly as Alison Heck.

--- solvedProblems/Capitalize.py
+++ solvedProblems/Capitalize.py
@@ -1,27 +1,28 @@
-# You are asked to ensure that the first and last names of people begin with a capital letter in their passports. For example, alison heck should be capitalised correctly as Alison Heck.
+"""
+Task:
+You are asked to ensure that the first and last names of people begin with a capital letter in their passports. For example, alison heck should be capitalised correctly as Alison Heck.
 
+Given a full name, your task is to capitalize the name appropriately.
 
-# Given a full name, your task is to capitalize the name appropriately.
+Input Format
 
-# Input Format
+A single line of input containing the full name, S.
 
-# A single line of input containing the full name, S.
+Constraints
 
-# Constraints
+The string consists of alphanumeric characters and spaces.
+Note: in a word only the first character is capitalized. Example 12abc when capitalized remains 12abc.
 
-# The string consists of alphanumeric characters and spaces.
-# Note: in a word only the first character is capitalized. Example 12abc when capitalized remains 12abc.
+Output Format
 
-# Output Format
+Print the capitalized string, S.
 
-# Print the capitalized string, S.
+Sample Input
 
-# Sample Input
+chris alan
+Sample Output
 
-# chris alan
-# Sample Output
-
-# Chris Alan    
+Chris Alan"""    
 
 # ================ Solution ===================== #
 # Complete the solve function below.
