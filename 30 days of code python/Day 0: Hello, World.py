48a735c4ce08e023905c5160e3939ac22ab6e235
Kamran khan <76246927+kamipakistan@users.noreply.github.com>
2022-12-21 23:12:22 +0500

    Solved
    To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World.
    on a single line, and finally, print the value of your variable on a second line.

---
+++ 30 days of code python/Day 0: Hello, World.py
@@ -0,0 +1,33 @@
+"""
+Task
+To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World.
+on a single line, and finally print the value of your variable on a second line.
+
+You've got this!
+
+Note: The instructions are Java-based, but we support submissions in many popular languages.
+You can switch languages using the drop-down menu above your editor,
+and the input_string variable may be written differently depending on the best-practice conventions of your submission language.
+
+Input Format
+A single line of text denoting  input_string (the variable whose contents must be printed).
+
+Output Format
+Print Hello, World. on the first line, and the contents of input_string on the second line.
+
+Sample Input
+Welcome to 30 Days of Code!
+
+Sample Output 
+Hello, World. 
+Welcome to 30 Days of Code!
+"""
+# =========================== Solved ================================
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+input_string = input()
+
+# Print a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
+print(input_string)
