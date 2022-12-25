8dc22a59e237504af609d6a1cb51c1ae58f2bedb
Kamran khan <76246927+kamipakistan@users.noreply.github.com>
2022-12-25 22:20:13 +0500

    Solved

--- 30 days of code python/Day 0: Hello, World.py
+++ 30 days of code python/Day 0: Hello, World.py
@@ -1,4 +1,8 @@
 """
+# =====================================================================
+# =========================== Day 0: Hello, World =====================
+# =====================================================================
+
 Task
 To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World.
 on a single line, and finally print the value of your variable on a second line.
