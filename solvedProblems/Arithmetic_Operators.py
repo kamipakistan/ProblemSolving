35d89261e0dd29fe0dbeeb3bb5244f9ebaf1e0fd
kamrankhan <kamrankhnkami@gmail.com>
2023-05-07 23:30:07 +0500

    Arthmetic Operations

---
+++ solvedProblems/Arithmetic_Operators.py
@@ -0,0 +1,13 @@
+# Checking if the code is being run as the main program
+if __name__ == '__main__':
+    # Getting input from user and converting to integer for variable a
+    a = int(input())
+    # Getting input from user and converting to integer for variable b
+    b = int(input())
+
+    # Adding and printing a and b
+    print(a + b)
+    # Subtracting and printing b from a
+    print(a - b)
+    # Multiplying and printing a and b
+    print(a * b)
