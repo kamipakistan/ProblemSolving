1d6e798979f5380a9d42feed1cb1667ae6f3dabe
kamrankhan <kamrankhnkami@gmail.com>
2023-05-07 23:34:12 +0500

    Division Operation

---
+++ solvedProblems/Division.py
@@ -0,0 +1,11 @@
+# Checking if the code is being run as the main program
+if __name__ == '__main__':
+    # Getting input from user and converting to integer for variable a
+    a = int(input())
+    # Getting input from user and converting to integer for variable b
+    b = int(input())
+
+    # Performing integer division on a and b, and printing the result
+    print(a // b)
+    # Dividing a by b, and printing the result as a float
+    print(a / b)
